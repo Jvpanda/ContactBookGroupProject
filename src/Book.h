74cdf911b8c8bef3a8bf809838ffbc12ea1821db
Jared Masters <mastersjared3@gmail.com>
2025-10-17 01:51:36 -0700

    add: book declaration

--- src/Book.h
+++ src/Book.h
@@ -1,8 +1,35 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+#include "contact.h"
+
 class Book
 {
+
+public:
+	const std::vector<Contact>& getContacts()const;
+
+	void addContact();
+	void addContact(const Contact& contact);
+
+	void deleteContact(int id);
+	void printIndividualDetails(int id)const;
+
+	void listContacts()const;
+	void listContactsByType(ContactType contactType)const;
+
+	void showContactsMissingInfo()const;
+
+	void searchContactByName(const std::string& name);
+	void searchContactByEmail(const std::string& email);
+	void searchContactByPhone(const std::string& phoneNumber);
+
+	void filterByType(ContactType contactType);
+	void filterByCity(const std::string& city);
+	void filterByTag(const std::string& tag);
+
+private:
+	std::vector<Contact> m_contacts;
 
 };
 
