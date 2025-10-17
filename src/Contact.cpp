9f07910b09d69723566e81619e1cacea4dd4fc78
Jared Masters <mastersjared3@gmail.com>
2025-10-17 15:18:36 -0700

    replaced contact header and cpp with Parker's

---
+++ src/Contact.cpp
@@ -0,0 +1,55 @@
+#include "Contact.h"
+using namespace std;
+
+int Contact::getId() const {
+	return id;
+}
+const std::string& Contact::getName() const {
+	return name;
+}
+const std::string& Contact::getEmail() const {
+	return email;
+}
+const std::string& Contact::getPhoneNumber() const {
+	return phoneNumber;
+}
+const std::string& Contact::getCity() const {
+	return city;
+}
+ContactType Contact::getType() const {
+	return type;
+}
+const std::vector<std::string>& Contact::getTags() const {
+	return tags;
+}
+
+void Contact::setId(int newId) {
+	id = newId;
+}
+void Contact::setName(const std::string& newName) {
+	name = newName;
+}
+void Contact::setEmail(const std::string& newEmail) {
+	email = newEmail;
+}
+void Contact::setPhoneNumber(const std::string& newNumber) {
+	phoneNumber = newNumber;
+}
+void Contact::setCity(const std::string& newCity) {
+	city = newCity;
+}
+void Contact::setType(ContactType newType) {
+	type = newType;
+}
+void Contact::addTag(const std::string& newTag) {
+	std::vector<std::string>::iterator it = find(tags.begin(), tags.end(), newTag);			// check if new tag already exists
+	if (it == tags.end()) {																	// if not, add tag
+		tags.push_back(newTag);
+	}
+}
+void Contact::removeTag(const std::string& tagToRemove) {
+	std::vector<std::string>::iterator it = find(tags.begin(), tags.end(), tagToRemove);	// locate desired tag if possible
+	if (it != tags.end()) {																	// remove if it exists
+		tags.erase(it);
+	}
+}
--- src/Contact.h
+++ src/Contact.h
@@ -1,54 +1,37 @@
-#ifndef CONTACT_H
-#define CONTACT_H
-
+#pragma once
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
-enum class ContactType
-{
-	Person = 1, Business, Vendor, Emergency
+enum ContactType { Person, Business, Vendor, Emergency };
+
+class Contact {
+	public:
+		// GETTERS
+		int getId() const;
+		const std::string& getName() const;
+		const std::string& getEmail() const;
+		const std::string& getPhoneNumber() const;
+		const std::string& getCity() const;
+		ContactType getType() const;
+		const std::vector<std::string>& getTags() const;
+
+		// SETTERS
+		void setId(int newId);
+		void setName(const std::string& newName);
+		void setEmail(const std::string& newEmail);
+		void setPhoneNumber(const std::string& newNumber);
+		void setCity(const std::string& newCity);
+		void setType(ContactType newType);
+		void addTag(const std::string& newTag);
+		void removeTag(const std::string& tagToRemove);
+
+	private:
+		int id;
+		std::string name;
+		std::string email;
+		std::string phoneNumber;
+		std::string city;
+		ContactType type;
+		std::vector<std::string> tags;
 };
-
-class Contact
-{
-
-public:
-	Contact(const std::string& name, const std::string& email, const std::string& phoneNumber,
-		const std::string& city, ContactType contactType);
-
-	void addTag(const std::string& tag);
-	void removeTag(const std::string& tag);
-	void printDetails()const;
-
-	int getId()const;
-	const std::string& getName()const;
-	const std::string& getEmail()const;
-	const std::string& getPhoneNumber()const;
-	const std::string& getCity()const;
-	ContactType getContactType()const;
-	std::string getContactTypeString()const;
-	const std::vector<std::string>& getTags()const;
-
-	void setName(const std::string& newName);
-	void setEmail(const std::string& newEmail);
-	void setPhoneNumber(const std::string& newPhoneNumber);
-	void setCity(const std::string& newCity);
-	void setContactType(ContactType newContactType);
-
-private:
-	int m_id;
-	std::string m_name;
-	std::string m_email;
-	std::string m_phoneNumber;
-	std::string m_city;
-	ContactType m_contactType;
-	std::vector<std::string> m_tags;
-
-private:
-	static int id;
-
-
-};
-
-#endif
